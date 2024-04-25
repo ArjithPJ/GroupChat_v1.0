1a5f0cac0c580b30f941fde578d53daba6778776
Arjith P J <pjarjith@gmail.com>
2024-04-25 15:19:11 +0530

    Forgot Password integrated

--- public/Login/forgotPassword.js
+++ public/Login/forgotPassword.js
@@ -5,7 +5,7 @@ async function forgotPassword(e){
         const verificationDetails = {
             email: email
         }
-        const response =await axios.post('http://44.206.240.170:3000/password/forgotpassword', verificationDetails);
+        const response =await axios.post('http://localhost:3000/password/forgotpassword', verificationDetails);
         if(response.status === 200){
             console.log("Email Sent");
             localStorage.setItem('email', JSON.stringify(email));
