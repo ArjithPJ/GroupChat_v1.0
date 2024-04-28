db13a7452fa7cb541a1e8f5937d5093c9d59cfec
Arjith P J <pjarjith@gmail.com>
2024-04-28 13:06:45 +0530

    Sockets added and updated

--- public/Home/groupList.js
+++ public/Home/groupList.js
@@ -54,7 +54,7 @@ groupList.addEventListener("click", async function(event) {
     const fetchChats = async () => {
         try {
             console.log("Socket called fetch chats!!");
-            const response = await axios.get(`http://localhost:3000/getChats?group_id=${groupId}&token=${token}`, {
+            const response = await axios.get(`http://23.22.247.49:3000/getChats?group_id=${groupId}&token=${token}`, {
                 validateStatus: function(status) {
                     return status >= 200 && status < 500; // Accept only status codes between 200 and 499
                 }
